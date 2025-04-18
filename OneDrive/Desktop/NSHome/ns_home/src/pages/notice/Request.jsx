278dc8d0bed2716b0d48c649c8b4e1848fd55b4d
Woneeee <a6885088@naver.com>
2025-04-18 17:29:21 +0900

    save

--- OneDrive/Desktop/NSHome/ns_home/src/pages/notice/Request.jsx
+++ OneDrive/Desktop/NSHome/ns_home/src/pages/notice/Request.jsx
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import request_bg from "../../img/request_bg.jpeg";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Container = styled.section`
   width: 100%;
@@ -7,7 +9,7 @@ const Container = styled.section`
 
 const Bg = styled.div`
   width: 100%;
-  height: 450px;
+  height: auto;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -16,12 +18,6 @@ const Bg = styled.div`
   background-position: center;
   background-size: cover;
   position: relative;
-  h1 {
-    font-size: 55px;
-    font-weight: 500;
-    letter-spacing: -1px;
-    position: absolute;
-  }
 `;
 
 const ImgBg = styled.div`
@@ -31,16 +27,173 @@ const ImgBg = styled.div`
   background-color: #d0d0d02c;
 `;
 
+const Form = styled.form`
+  padding: 50px 10px;
+  max-width: 500px;
+  width: 100%;
+  border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  background-color: #cdcdcdb1;
+  backdrop-filter: blur(5px);
+  margin: 150px 0 50px 0;
+  h1 {
+    font-size: 55px;
+    font-weight: 500;
+    letter-spacing: -1px;
+    text-align: center;
+  }
+  p {
+    width: 250px;
+    font-size: 14px;
+    font-weight: 300;
+    line-height: 22px;
+    opacity: 0.7;
+    margin: 0 auto;
+    margin-top: 25px;
+    text-align: center;
+  }
+  h4 {
+    font-size: 14px;
+    font-weight: 300;
+    margin-top: 17px;
+  }
+  span {
+    color: #ff0000;
+  }
+  input {
+    all: unset;
+    width: 100%;
+    height: 40px;
+    border: 1px solid #55555530;
+    margin-top: 4px;
+    border-radius: 13px;
+    padding: 0 10px;
+    box-sizing: border-box;
+    font-size: 14px;
+  }
+  textarea {
+    all: unset;
+    display: block;
+    width: 100%;
+    height: 250px;
+    border: 1px solid #55555530;
+    margin-top: 4px;
+    border-radius: 13px;
+    padding: 10px;
+    resize: none;
+    font-size: 14px;
+    box-sizing: border-box;
+    white-space: pre-wrap;
+    overflow-wrap: break-word;
+    word-break: break-word;
+  }
+`;
+
+const ErrorMessage = styled.div`
+  font-size: 14px;
+  color: #ff0000;
+  font-weight: 300;
+  margin-top: 5px;
+`;
+
+const Btn = styled.button`
+  margin-top: 40px;
+  width: 100%;
+  height: 50px;
+  background-color: #0a0a539b;
+  border-radius: 13px;
+  cursor: pointer;
+  color: #fff;
+  font-weight: 300;
+`;
+
 export const Request = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const requestHandler = () => {};
+
   return (
     <Container>
       <Bg>
         <ImgBg />
-        <h1>고객 문의</h1>
+
+        <Form onSubmit={handleSubmit(requestHandler)}>
+          <h1>고객 문의</h1>
+          <p>
+            상황을 구체적으로 말씀해주시면, 적합한 상담과 제안을 드리겠습니다.
+          </p>
+
+          <h4>
+            담당자명&nbsp;<span>*</span>
+          </h4>
+          <input
+            {...register("name", {
+              required: "답변을 입력해주세요",
+            })}
+            type="text"
+            style={{ borderColor: errors?.name ? "red" : "#00000027" }}
+          />
+          <ErrorMessage>{errors?.name?.message}</ErrorMessage>
+
+          <h4>
+            소속(회사명)&nbsp;<span>*</span>
+          </h4>
+          <input
+            {...register("company", {
+              required: "답변을 입력해주세요",
+            })}
+            type="text"
+            style={{ borderColor: errors?.company ? "red" : "#00000027" }}
+          />
+          <ErrorMessage>{errors?.company?.message}</ErrorMessage>
+
+          <h4>
+            연락처&nbsp;<span>*</span>
+          </h4>
+          <input
+            {...register("contact", {
+              required: "답변을 입력해주세요",
+            })}
+            type="tel"
+            style={{ borderColor: errors?.contact ? "red" : "#00000027" }}
+          />
+          <ErrorMessage>{errors?.contact?.message}</ErrorMessage>
+
+          <h4>
+            회사 이메일&nbsp;<span>*</span>
+          </h4>
+          <input
+            {...register("email", {
+              required: "답변을 입력해주세요",
+            })}
+            type="text"
+            style={{ borderColor: errors?.email ? "red" : "#00000027" }}
+          />
+          <ErrorMessage>{errors?.email?.message}</ErrorMessage>
+
+          <h4>
+            문의내용&nbsp;<span>*</span>
+          </h4>
+          <textarea
+            className="contents"
+            {...register("contents", {
+              required: "답변을 입력해주세요",
+            })}
+            type="text"
+            style={{ borderColor: errors?.contents ? "red" : "#00000027" }}
+          />
+          <ErrorMessage>{errors?.contents?.message}</ErrorMessage>
+
+          <Btn>제출하기</Btn>
+        </Form>
       </Bg>
     </Container>
   );
 };
 
-//년도를 최신순
-//해당사례가 없는 년도는 없애기
+//
